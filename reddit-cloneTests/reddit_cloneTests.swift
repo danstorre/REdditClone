3adc66b6fc40111f814a00e90f9ecaeb83c1d1a6
Daniel Torres <danstorre@gmail.com>
2020-01-22 20:50:00 -0300

    add parsing test for listpost

--- reddit-cloneTests/reddit_cloneTests.swift
+++ reddit-cloneTests/reddit_cloneTests.swift
@@ -10,25 +10,30 @@ import XCTest
 @testable import reddit_clone
 
 class reddit_cloneTests: XCTestCase {
-
+    
+    var json: Data?
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        if let path = Bundle.main.path(forResource: "topsample", ofType: "json") {
+            do {
+                json = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            } catch {
+                fatalError("error reading topsample.json file")
+            }
         }
     }
-
+    
+    override func tearDown() {
+        json = nil
+    }
+    
+    func testExample() {
+        guard let json = json else { return XCTFail("no sameple posts json available ")}
+        let jsondecoder = JSONDecoder()
+        guard let listpost = try? jsondecoder.decode(ListPost.self, from: json) else {
+            return
+        }
+        XCTAssertEqual(listpost.posts[0].title, "Man trying to return a dog's toy gets tricked into playing fetch")
+    }
+    
 }
