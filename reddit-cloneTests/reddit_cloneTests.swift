adb345fcc0e95adc7ab68da1047dc72381c2d79e
Daniel Torres <danstorre@gmail.com>
2020-01-22 18:54:47 -0300

    add initial row posts interface, add colors.

---
+++ reddit-cloneTests/reddit_cloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  reddit_cloneTests.swift
+//  reddit-cloneTests
+//
+//  Created by Daniel Torres on 1/22/20.
+//  Copyright © 2020 dansTeam. All rights reserved.
+//
+
+import XCTest
+@testable import reddit_clone
+
+class reddit_cloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
