5b21bb58089dcf259d2c0556a662dd6ef9cef9f0
Daniel Torres <danstorre@gmail.com>
2020-01-22 21:51:12 -0300

    add readable data for posts

--- reddit-clone/Model/PostView.swift
+++ reddit-clone/Model/PostView.swift
@@ -22,17 +22,52 @@ struct PostView: Readable, Deletable, Hashable{
     var isDeleted: Bool
     let identifier = UUID()
     
+    var readableDate: String {
+        return post.entryDate.getElapsedInterval()
+    }
+    
+    var numberOfComments: String {
+        return "\(post.comments) comments"
+    }
+    
+    var minimumDescription: String {
+        return post.title.components(separatedBy: ".").first ?? post.title
+    }
+    
     init(post: Post, isRead: Bool) {
         self.post = post
         self.isRead = isRead
         self.isDeleted = false
     }
-
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(identifier)
     }
-
+    
     static func ==(lhs: PostView, rhs: PostView) -> Bool {
         return lhs.post == rhs.post
+    }
+}
+
+extension Date {
+    
+    func getElapsedInterval() -> String {
+        
+        let interval = Calendar.current.dateComponents([.year, .month, .day], from: self, to: Date())
+        
+        if let year = interval.year, year > 0 {
+            return year == 1 ? "\(year)" + " " + "year ago" :
+                "\(year)" + " " + "years ago"
+        } else if let month = interval.month, month > 0 {
+            return month == 1 ? "\(month)" + " " + "month ago" :
+                "\(month)" + " " + "months ago"
+        } else if let day = interval.day, day > 0 {
+            return day == 1 ? "\(day)" + " " + "day ago" :
+                "\(day)" + " " + "days ago"
+        } else {
+            return "a moment ago"
+            
+        }
+        
     }
 }
